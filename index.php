03bbdb7c5970b0c85e4bd3bc3e9cfe6772366878
emersonnobre <egrnobre@gmail.com>
2021-03-25 18:05:39 -0300

    working in login admin

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use\Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\DB\Sql;
 
 $app = new Slim();
 
@@ -21,6 +23,47 @@ $app->get('/admin', function() {
     
 	$page = new PageAdmin();
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin($opts = [
+		'header'=>false,
+		'footer'=>false
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/inserir', function() {
+
+	$sql = new Sql();
+	$data = date('Y-m-d');
+	define('SECRET_IV', pack('a16', 'acesso'));
+	define('SECRET', pack('a16', 'acesso'));
+	$senha = 'oi';
+	$openssl = openssl_encrypt(
+		$senha,
+		'AES-128-CBC',
+		SECRET,
+		0,
+		SECRET_IV
+	);
+
+	$sql->query("insert into tb_users values (3, 3, 'biel', :OPEN, 0,  :DATA)", array(
+		':OPEN'=>$openssl,
+		':DATA'=>$data
+	));
 
 });
 
